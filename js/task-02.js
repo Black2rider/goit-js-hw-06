d43baa4ba52a07a0823fd7caf67a1c943bafc59f
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2023-03-26 23:59:45 +0300

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients.map(el => {
+  const items = document.createElement("li");
+  items.textContent = el;
+  items.classList.add("item");
+  return items;
+});
+
+const inserting = list.append(...elements);
