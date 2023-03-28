501090668992e2b32715836c20856d41d85f9fc6
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2023-03-28 15:42:46 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+const submitForm = form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  //console.log(event.currentTarget)
+   const {
+    elements: { login, password }
+  } = event.currentTarget;
+
+  if (login.value === "" || password.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+  
+  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+    
+  
+}
 
