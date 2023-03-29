490d66aa0f89a2d582c6bb483fba6f22d1f2f522
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2023-03-29 15:38:48 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const spanWidget = document.querySelector(".color");
+const butClick = button.addEventListener("click", onClick);
+
+function onClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  spanWidget.textContent = body.style.backgroundColor;
+}
