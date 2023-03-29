980669917c166ccd4a99b94850fb76e1884dce67
Kostya Kusik <60221585+Black2rider@users.noreply.github.com>
2023-03-29 15:52:24 +0300

    task 8reduct

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const create =  document.querySelector("button[data-create]");
+const createListener = create.addEventListener("click", onClick);
+
+function onClick(event) {
+  console.dir(event.currentTarget)
+}
+
+function createBoxes(amount) {
+
+}
